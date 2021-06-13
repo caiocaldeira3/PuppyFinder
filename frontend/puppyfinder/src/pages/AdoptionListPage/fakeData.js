c337fe3abc8885e65e80004b62b194947cd96931
Gustavo Dias de Oliveira <dias.gdo@gmail.com>
2021-06-13 20:22:16 -0300

    primeiro prototipo da lista com dados fake

---
+++ frontend/puppyfinder/src/pages/AdoptionListPage/fakeData.js
@@ -0,0 +1,7 @@
+const fakeDataAdoptionList = new Array(50).fill({
+  nome: 'Nome do dog',
+  localizacao: 'Centro - Belo Horizonte',
+  descricao: 'Uma descrição sobre o dog e como ele foi encontrado - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempor tortor at elit posuere, in vulputate turpis tincidunt. Cras non augue condimentum, blandit eros sed, facilisis leo. Morbi nunc velit, aliquet id erat eu, consectetur sodales enim. Morbi at nisi vitae ipsum dapibus viverra. Maecenas interdum eleifend odio, vitae faucibus nibh rutrum ut.',
+});
+
+export default fakeDataAdoptionList;
--- frontend/puppyfinder/src/pages/AdoptionListPage/index.jsx
+++ frontend/puppyfinder/src/pages/AdoptionListPage/index.jsx
@@ -1,9 +1,71 @@
 import React from 'react';
+import {
+  Container,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  makeStyles,
+} from '@material-ui/core';
 
-const AdoptionListPage = () => (
-  <div>
-    This is the main page
-  </div>
-);
+import fakeDataAdoptionList from './fakeData';
+import dogImage from './assets/dog.jpeg';
+
+const useStyles = makeStyles(() => ({
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%',
+  },
+}));
+
+const AdoptionListPage = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Container>
+        <Grid container spacing={4}>
+          { fakeDataAdoptionList.map((item) => (
+            <Grid item key={item} xs={12} sm={6} md={4}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image={dogImage}
+                  title="Dog"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {item.nome}
+                  </Typography>
+                  <Typography gutterBottom variant="subtitle2" color="textSecondary">
+                    {item.localizacao}
+                  </Typography>
+                  <Typography>
+                    {item.descricao}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    View
+                  </Button>
+                  <Button size="small" color="primary">
+                    Edit
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
+};
 
 export default AdoptionListPage;
