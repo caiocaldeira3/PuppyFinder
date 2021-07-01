83aa36fc81587564f2606df3a790ae08dd781324
root <root@DESKTOP-JAB9QK4.localdomain>
2021-06-30 23:23:11 -0300

    Adicionar form de criação de animal

--- app/modules/org/forms.py
+++ app/modules/org/forms.py
@@ -1,3 +1,32 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, PasswordField
-from wtforms.validators import Required, Email
+from wtforms import TextField, PasswordField, RadioField, SelectField, IntegerField
+from wtforms.validators import Required, Email, InputRequired
+
+class RegisterAnimalForm (FlaskForm):
+    name        = TextField(
+        "Nome", [Required(message="É obrigatório fornecer seu nome")]
+    )
+    age         = IntegerField("Idade")
+    sex         = SelectField(
+        "Sexo",
+        choices=[],
+        validators=[InputRequired()]
+    )  
+    fur         = TextField("Pelagem")
+    size        = SelectField("Sexo", choices=[]) 
+    neutered    = RadioField(
+        "Castrado",
+        choices = [(True,'Sim'),(False,'Não')],
+        validators=[InputRequired("É obrigatório informar se castrado")]
+    )
+    vaccinated    = RadioField(
+        "Vacinado",
+        choices = [(True,'Sim'),(False,'Não')],
+        validators=[InputRequired("É obrigatório informar se vacinado")]
+    )
+    dewormed    = RadioField(
+        "Vermifugado",
+        choices = [(True,'Sim'),(False,'Não')],
+        validators=[InputRequired("É obrigatório informar se vermifugado")]
+    )
+    desc         = TextField("Descrição")
