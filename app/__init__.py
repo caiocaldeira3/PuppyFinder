54b589387d43b7b979e4dd36aacc0d915a1a3c8c
caiocaldeira3 <caio.caldeira3@gmail.com>
2021-07-04 17:27:20 -0300

    Adicionar módulo de requisições para usuários

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 # Import flask and template operators
-from flask import Flask, render_template
+from flask import Flask, wrappers
 
 # Import SQLAlchemy
 from flask_sqlalchemy import SQLAlchemy
 
 # Import Migration Module
 from flask_migrate import Migrate
+
+# Import Util Modules
+from app.util.responses import NotFoundError
 
 # Define the WSGI application object
 app = Flask(__name__)
@@ -20,20 +23,22 @@ migrate = Migrate(app, db)
 
 # Sample HTTP error handling
 @app.errorhandler(404)
-def not_found (error: Exception) -> str:
-    return render_template('404.html'), 404
+def not_found (error: Exception) -> wrappers.Response:
+    return NotFoundError
 
 # Import a module / component using its blueprint handler variable (mod_auth)
 
 from app.modules.animal.controllers import mod_anm as anm_module
 from app.modules.auth.controllers import mod_auth as auth_module
 from app.modules.org.controllers import mod_org as org_module
+from app.modules.user.controllers import mod_user as user_module
 
 
 # Register blueprint(s)
 app.register_blueprint(auth_module)
 app.register_blueprint(org_module)
 app.register_blueprint(anm_module)
+app.register_blueprint(user_module)
 # app.register_blueprint(xyz_module)
 # ..
 
